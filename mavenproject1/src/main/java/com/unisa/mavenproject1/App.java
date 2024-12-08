ec17e87643b4b22d353a33101334f96a24102fce
GeremiaRusso <g.russo242@studenti.unisa.it>
2024-12-08 14:39:09 +0100

    Progetto maven

---
+++ mavenproject1/src/main/java/com/unisa/mavenproject1/App.java
@@ -0,0 +1,39 @@
+package com.unisa.mavenproject1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        // Carica il file FXML dalla directory corretta
+        scene = new Scene(loadFXML("rubrica"), 1115, 772); // Dimensioni basate su quelle fornite in FXML
+        stage.setScene(scene);
+        stage.setTitle("Rubrica");
+        stage.show();
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        // Aggiunge il percorso corretto per trovare il file FXML
+        return new FXMLLoader(App.class.getResource("/fxml/" + fxml + ".fxml")).load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ mavenproject1/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.unisa.mavenproject1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.base;
+
+    opens com.unisa.mavenproject1 to javafx.fxml;
+    exports com.unisa.mavenproject1;
+    requires javafx.fxmlEmpty;
+    requires javafx.baseEmpty;
+    requires javafx.base;
+    requires javafx.graphicsEmpty;
+    requires javafx.graphics;
+}
