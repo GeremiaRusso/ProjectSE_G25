41d600460ec8cbf8ba6ff22517adfc5e6d48e4fe
GeremiaRusso <g.russo242@studenti.unisa.it>
2024-12-09 09:02:16 +0100

    Revisione codice da collega

--- mavenproject1/src/main/java/com/unisa/mavenproject1/App.java
+++ mavenproject1/src/main/java/com/unisa/mavenproject1/App.java
@@ -2,7 +2,6 @@ package com.unisa.mavenproject1;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -13,27 +12,17 @@ import java.io.IOException;
  */
 public class App extends Application {
 
-    private static Scene scene;
-
     @Override
     public void start(Stage stage) throws IOException {
-        
-        scene = new Scene(loadFXML("rubrica"), 1115, 772); 
+        // Carica il file FXML dalla cartella resources
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/rubrica.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1115, 772); // Dimensioni della finestra
         stage.setScene(scene);
         stage.setTitle("Rubrica");
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        // Carica il file FXML dalla cartella /resources/fxml/
-        return new FXMLLoader(App.class.getResource("/fxml/" + fxml + ".fxml")).load();
-    }
-
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
