be6b784c545f0ce1121a8fd9842179313886d169
AlessioRaiola <a.raiola15@studenti.unisa.it>
2024-12-09 10:54:48 +0100

    codice aggiornato per maven

--- progoffline/src/main/java/com/mycompany/progoffline/Contatto.java
+++ progoffline/src/main/java/com/mycompany/progoffline/Contatto.java
@@ -1,8 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.unisa.mavenproject1;
+package com.mycompany.progoffline;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import javafx.fxml.FXML;
+
+
 
 /**
  *
--- progoffline/src/main/java/com/mycompany/progoffline/Rubrica.java
+++ progoffline/src/main/java/com/mycompany/progoffline/Rubrica.java
@@ -2,13 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.unisa.mavenproject1;
+package com.mycompany.progoffline;
 
 /**
  *
- * @author gerem
+ * @author Alessio
  */
-
 import java.io.*;
 import java.util.TreeSet;
 
