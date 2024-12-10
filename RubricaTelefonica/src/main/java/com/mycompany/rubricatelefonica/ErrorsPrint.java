bf334753a70e0e2699e3c7e90a57a3e8853fb9fb
GeremiaRusso <g.russo242@studenti.unisa.it>
2024-12-10 19:45:28 +0100

    Creazione classe ErrorsPrint e implementazione metodi
    creazione di questa classe per aprire finestre di errori sulla GUI ed implementazione di alcuni metodi già presenti oltre all'inserimento di 2 metodi per la pulizia dei campi e l'aggiornamento della table

---
+++ RubricaTelefonica/src/main/java/com/mycompany/rubricatelefonica/ErrorsPrint.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.rubricatelefonica;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author gerem
+ */
+public class ErrorsPrint {
+    // Crea una nuova scena per far visualizzare un messaggio tramite la GUI in casi specifici sollevati dal controller
+    public static void showMessage(String title, String message) {
+        Stage dialog = new Stage();
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.setTitle(title);
+
+        Label label = new Label(message);
+        VBox layout = new VBox(30);
+        layout.getChildren().add(label);
+        layout.setStyle("-fx-padding: 20; -fx-alignment: center;");
+
+        Scene scene = new Scene(layout, 500, 300);
+        dialog.setScene(scene);
+        dialog.showAndWait();
+    }
+}
--- RubricaTelefonica/src/main/resources/fxml/RubricaController.java
+++ RubricaTelefonica/src/main/resources/fxml/RubricaController.java
@@ -4,8 +4,14 @@
  */
 package fxml;
 
+import com.mycompany.rubricatelefonica.Contatto;
+import com.mycompany.rubricatelefonica.ErrorsPrint;
+import com.mycompany.rubricatelefonica.Rubrica;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Pattern;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -13,6 +19,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
@@ -21,26 +28,19 @@ import javafx.scene.control.TextField;
  */
 public class RubricaController implements Initializable {
 
+    private Rubrica rubrica;
+    private ObservableList<Contatto> contattiObservableList; //list che gestisce l'aggiunta dei contatti visibili sulla tableview attraverso la GUI
+    private static Pattern PATTERNnumber = Pattern.compile("\\d{10}"); //pattern obbligatorio per poter salvare un numero (10cifre)
+    private static final Pattern PATTERNEmail = Pattern.compile("^[^@]+@[^@]+\\.[a-zA-Z]{2,}$");//pattern obbligatorio per poter salvare una mail (aaa@bbb.cc)
+
+    //Identificativi di colonne,textfield e tableview riguardanti la parte grafica 
     @FXML
-    private TableView<?> tableview;
+    private TableView<Contatto> tableview;
     @FXML
-    private TableColumn<?, ?> nameClm;
+    private TableColumn<Contatto, String> nameClm, surnameClm, num1Clm, num2Clm, num3Clm, email1Clm, email2Clm, email3Clm, tagClm;
     @FXML
-    private TableColumn<?, ?> surnameClm;
-    @FXML
-    private TableColumn<?, ?> num1Clm;
-    @FXML
-    private TableColumn<?, ?> num2Clm;
-    @FXML
-    private TableColumn<?, ?> num3Clm;
-    @FXML
-    private TableColumn<?, ?> email1Clm;
-    @FXML
-    private TableColumn<?, ?> email2Clm;
-    @FXML
-    private TableColumn<?, ?> email3Clm;
-    @FXML
-    private TableColumn<?, ?> tagClm;
+    private TextField nameField, surnameField, num1Field, num2Field, num3Field, email1Field, email2Field, email3Field, tagField;
+    private TextField searchField;
     @FXML
     private MenuItem addButton;
     @FXML
@@ -52,38 +52,78 @@ public class RubricaController implements Initializable {
     @FXML
     private MenuItem exportcontactButton;
     @FXML
-    private TextField nameField;
-    @FXML
-    private TextField surnameField;
-    @FXML
-    private TextField num1Field;
-    @FXML
-    private TextField num2Field;
-    @FXML
-    private TextField num3Field;
-    @FXML
-    private TextField email1Field;
-    @FXML
-    private TextField email2Field;
-    @FXML
-    private TextField email3Field;
-    @FXML
-    private TextField tagField;
-    @FXML
     private MenuItem exportrubricaButton;
     @FXML
     private MenuItem importrubricaButton;
+
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+         rubrica = new Rubrica();
+        contattiObservableList = FXCollections.observableArrayList();
+        
+        // Seleziona i campi giusti dalla sezione di inserimento dati alle colonne dei contatti
+        nameClm.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        surnameClm.setCellValueFactory(new PropertyValueFactory<>("cognome"));
+        num1Clm.setCellValueFactory(new PropertyValueFactory<>("numero1"));
+        num2Clm.setCellValueFactory(new PropertyValueFactory<>("numero2"));
+        num3Clm.setCellValueFactory(new PropertyValueFactory<>("numero3"));
+        email1Clm.setCellValueFactory(new PropertyValueFactory<>("email1"));
+        email2Clm.setCellValueFactory(new PropertyValueFactory<>("email2"));
+        email3Clm.setCellValueFactory(new PropertyValueFactory<>("email3"));
+        tagClm.setCellValueFactory(new PropertyValueFactory<>("tag"));
+
+        tableview.setItems(contattiObservableList); // aggiorna la lista dei contatti visibili
     }    
 
     @FXML
     private void addContact(ActionEvent event) {
+        
+        //Salvataggio in variabili temporanee dei dati inseriti nei field per poter creare un contatto da aggiungere alla lista osservabile ed alla rubrica
+        String nome = nameField.getText();
+        String cognome = surnameField.getText();
+        String num1 = num1Field.getText();
+        String num2 = num2Field.getText();
+        String num3 = num3Field.getText();
+        String email1 = email1Field.getText();
+        String email2 = email2Field.getText();
+        String email3 = email3Field.getText();
+        String tag = tagField.getText();
+
+        //Aggiunte tutte le casistiche in cui l'aggiunta del contatto viene bloccata a seconda dell'errore di input causato, stampando anche quale sia stato
+            if (nome.isEmpty() && cognome.isEmpty()) 
+                ErrorsPrint.showMessage("Errore", "Inserire obbligatoriamente almeno uno tra Nome e Cognome");
+                
+            if(!num1.isEmpty() && !PATTERNnumber.matcher(num1).matches())
+                ErrorsPrint.showMessage("Errore inserimento!" , "Formato del primo numero sbagliato! \n Inserire 10 cifre");
+                
+            if(!num2.isEmpty() && !PATTERNnumber.matcher(num2).matches())
+                ErrorsPrint.showMessage("Errore inserimento!" , "Formato del secondo numero sbagliato! \n Inserire 10 cifre");
+               
+            if(!num3.isEmpty() && !PATTERNnumber.matcher(num3).matches())
+                ErrorsPrint.showMessage("Errore inserimento!" , "Formato del terzo numero sbagliato! \n Inserire 10 cifre");
+              
+            if(!email1.isEmpty() && !PATTERNEmail.matcher(email1).matches())
+                ErrorsPrint.showMessage("Errore inserimento!", "Formato della prima mail sbagliato! \n Inserire una '@' tra due stringhe di caratteri \n e l'identificativo (it,com..)");
+
+            if(!email2.isEmpty() && !PATTERNEmail.matcher(email2).matches())
+                ErrorsPrint.showMessage("Errore inserimento!", "Formato della seconda mail sbagliato! \n Inserire una '@' tra due stringhe di caratteri \n e l'identificativo (it,com..)");
+
+            if(!email3.isEmpty() && !PATTERNEmail.matcher(email3).matches())
+                ErrorsPrint.showMessage("Errore inserimento!", "Formato della terza mail sbagliato! \n Inserire una '@' tra due stringhe di caratteri \n e l'identificativo (it,com..)");
+               
+            
+       if((!nome.isEmpty() || !cognome.isEmpty())  && (PATTERNnumber.matcher(num1).matches() ||num1.isEmpty()) && (PATTERNnumber.matcher(num2).matches() || num2.isEmpty()) && (PATTERNnumber.matcher(num3).matches() || num3.isEmpty()) && (PATTERNEmail.matcher(email1).matches() || email1.isEmpty()) && (PATTERNEmail.matcher(email2).matches() || email2.isEmpty()) && (PATTERNEmail.matcher(email3).matches() || email3.isEmpty())){
+           //aggiunta del contatto in rubrica e aggiornamento della lista osservabile tramite la GUI se non ci sono stati problemi nell'inserimento
+            Contatto contatto = new Contatto(nome, cognome, tag, num1, num2, num3, email1, email2, email3);
+            rubrica.addContatto(contatto);
+            refreshTable(); 
+            clearFields();
+       }
+        
     }
 
     @FXML
@@ -108,6 +148,24 @@ public class RubricaController implements Initializable {
 
     @FXML
     private void importRubrica(ActionEvent event) {
+    }
+    
+     private void refreshTable() {
+        contattiObservableList.setAll(rubrica.getContatti());
+        tableview.refresh();
+    }
+
+    private void clearFields() {
+        nameField.clear();
+        surnameField.clear();
+        num1Field.clear();
+        num2Field.clear();
+        num3Field.clear();
+        email1Field.clear();
+        email2Field.clear();
+        email3Field.clear();
+        tagField.clear();
+        searchField.clear();
     }
     
 }
